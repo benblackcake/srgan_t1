26707e1c0e707b8ec053806de0b04f794efbd58b
ben <pingheng.chang@gmail.com>
2020-03-03 23:29:43 +0800

    modify log default 1000

--- train_test.py
+++ train_test.py
@@ -21,7 +21,7 @@ def main():
     parser.add_argument('--name', type=str, help='Name of experiment.')
     parser.add_argument('--overfit', action='store_true', help='Overfit to a single image.')
     parser.add_argument('--batch-size', type=int, default=16, help='Mini-batch size.')
-    parser.add_argument('--log-freq', type=int, default=10000,
+    parser.add_argument('--log-freq', type=int, default=1000,
                         help='How many training iterations between validation/checkpoints.')
     parser.add_argument('--learning-rate', type=float, default=1e-4, help='Learning rate for Adam.')
     parser.add_argument('--content-loss', type=str, default='mse', choices=['mse', 'vgg22', 'vgg54'],
